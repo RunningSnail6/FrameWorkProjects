08aef2df0b895ca39ec99a21e5372038566f4e4a
RunningSnail6 <959890568@qq.com>
2016-05-22 23:04:07 +0800

    no message

---
+++ Assets/Scripts/TeachEqual.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeachEqual : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+        string tempA = new string(new char[4]('H','E','L','O'));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
