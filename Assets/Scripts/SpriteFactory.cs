b1d65dd134e5d55476fe8ebd10609a80f82dcf13
RunningSnail6 <959890568@qq.com>
2016-05-27 23:53:06 +0800

    no message

--- Assets/Scripts/SpriteFactory.cs
+++ Assets/Scripts/SpriteFactory.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SpriteFactory : MonoBehaviour {
 
+    public object[] allSprite;
+
 	// Use this for initialization
 	void Start () {
-	
+        allSprite = Resources.LoadAll("Number");
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/TeachEqual.cs
+++ Assets/Scripts/TeachEqual.cs
@@ -1,17 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TeachEqual : MonoBehaviour {
+public class Vecter
+{
+    string name;
+    public Vecter(string tempName)
+    {
+        name = tempName;
+    }
+}
 
-	// Use this for initialization
-	void Start () {
-	    
-        string tempA = new string(new char[4]('H','E','L','O'));
+public class TeachEqual : MonoBehaviour
+{
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+        string tempA = new string(new char[] { 'H','L'});
+        string tempB = new string(new char[] { 'H', 'L'});
+
+        Debug.Log(tempA == tempB);
+
+        Debug.Log(tempA.Equals(tempB));
+
+        object tempC = tempA;
+        object tempD = tempB;
+
+        Debug.Log(tempC == tempD);
+        Debug.Log(tempC);
+
+        Debug.Log(tempC.Equals(tempD));
+
+        Debug.Log("Test Vecter");
+
+        Vecter tempOne = new Vecter("Hello");
+        Vecter tempTwo = new Vecter("Hello");
+
+        Debug.Log(tempOne == tempTwo);
+        Debug.Log(tempOne.Equals(tempTwo));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
