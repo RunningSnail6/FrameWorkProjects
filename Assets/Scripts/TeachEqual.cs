d9fc45d36e3c99bacd4d2a7fded9c7db40d2d762
jingmengshen <959890568@qq.com>
2016-05-31 21:04:16 +0800

    no message

--- Assets/Scripts/TeachEqual.cs
+++ Assets/Scripts/TeachEqual.cs
@@ -17,29 +17,43 @@ public class TeachEqual : MonoBehaviour
     void Start()
     {
 
-        string tempA = new string(new char[] { 'H','L'});
-        string tempB = new string(new char[] { 'H', 'L'});
-
-        Debug.Log(tempA == tempB);
-
-        Debug.Log(tempA.Equals(tempB));
-
-        object tempC = tempA;
-        object tempD = tempB;
-
-        Debug.Log(tempC == tempD);
-        Debug.Log(tempC);
-
-        Debug.Log(tempC.Equals(tempD));
-
-        Debug.Log("Test Vecter");
-
-        Vecter tempOne = new Vecter("Hello");
-        Vecter tempTwo = new Vecter("Hello");
-
-        Debug.Log(tempOne == tempTwo);
-        Debug.Log(tempOne.Equals(tempTwo));
+       
     }
+
+	//按值传递值 类型
+	public void Food(int x) 
+	{
+		x = 10;
+	}
+
+	public void Food2()
+
+
+	void TestEqual()
+	{
+		string tempA = new string(new char[] { 'H','L'});
+		string tempB = new string(new char[] { 'H', 'L'});
+		
+		Debug.Log(tempA == tempB);
+		
+		Debug.Log(tempA.Equals(tempB));
+		
+		object tempC = tempA;
+		object tempD = tempB;
+		
+		Debug.Log(tempC == tempD);
+		Debug.Log(tempC);
+		
+		Debug.Log(tempC.Equals(tempD));
+		
+		Debug.Log("Test Vecter");
+		
+		Vecter tempOne = new Vecter("Hello");
+		Vecter tempTwo = new Vecter("Hello");
+		
+		Debug.Log(tempOne == tempTwo);
+		Debug.Log(tempOne.Equals(tempTwo));
+	}
 
     // Update is called once per frame
     void Update()
