0b919445252db9cbbbdbe722a83c66ff8f9483cb
jingmengshen <959890568@qq.com>
2016-06-12 21:13:58 +0800

    no message

---
+++ Assets/FrameWork/Editor/AssetBundleEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AssetBundleEditor 
+{
+    [MenuItem("Itools/BuildeAssetBundle")]
+    public  static void BuildeAssetBundle()
+    {
+        string outPath = Application.dataPath + "/AssetBuldle";
+
+        BuildPipeline.BuildAssetBundle(outPath,0,EditorUserBuildSettings.activeBuildTarget);
+    }
+}
